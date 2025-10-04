82fe1f13df9e6d4f62f6e355f2db4ed52eadefe9
Mohamed Ali <91525367+mohamed-ali-elsayed@users.noreply.github.com>
2025-10-04 12:19:12 +0300

    Update 08-kill_zombies.sh

--- 08-kill_zombies.sh
+++ 08-kill_zombies.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # Script to find zombie processes and kill their parent process
 
-# Get the PID of the first zombie process
-Z_PID=$(ps aux | grep defunct | grep -v grep | awk '{ print $2 }')
+# Loop as long as there are zombie processes
+while [ $(ps aux | grep defunct | grep -v grep | wc -l) -gt 0 ]
+do
+    # Get the PID of the first zombie process
+    Z_PID=$(ps aux | grep defunct | grep -v grep | awk '{ print $2 }')
 
-# Get the parent PID of the zombie process
-Z_PPID=$(ps lax | awk -v zpid="$Z_PID" '$3 == zpid { print $4 }')
+    # Get the parent PID of the zombie process
+    Z_PPID=$(ps lax | awk -v zpid="$Z_PID" '$3 == zpid { print $4 }')
 
-# Kill the parent process to remove the zombie
-kill -9 "$Z_PPID"
+    # Kill the parent process to remove the zombie
+    kill -9 "$Z_PPID"
+
+    # Show what was killed
+    echo "Killed parent process $Z_PPID of zombie $Z_PID"
+done
